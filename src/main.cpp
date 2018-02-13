8a33a490de7744b25791fb75851056fb5d4fe2be
Will Eccles <ghostyx123@gmail.com>
2018-02-13 14:59:34 -0500

    getting started

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "vector3.h"
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+using namespace std::chrono; // for clocking every second
+
+
+int main(void) {
+	/* TODO:
+	 * 1. read input somehow, not sure yet
+	 * 2. setup simulation
+	 * 3. run simulation
+	 * 4. spit out results
+	 */
+};
---
+++ src/vector3.h
@@ -0,0 +1,56 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+class vector3 {
+	private:
+		// x, y, z (z being "up")
+		long double components[3] = {0.0l, 0.0l, 0.0l};
+	public:
+		vector3(long double x, long double y, long double z) {
+			components[0] = x;
+			components[1] = y;
+			components[2] = z;
+		};
+		vector3() {};
+		
+		/* Getters/Setters */
+		void setX(long double x) { components[0] = x; };
+		void setY(long double y) { components[1] = y; };
+		void setZ(long double z) { components[2] = z; };
+		long double getX() { return components[0]; };
+		long double getY() { return components[1]; };
+		long double getZ() { return components[2]; };
+
+		/* Vector Operators */
+		vector3& operator= (const vector3& rhs) {
+			if (this == &rhs) return *this; // check to make sure it's not beign self-assigned
+			components[0] = rhs.getX();
+			components[1] = rhs.getY();
+			components[2] = rhs.getZ();
+			return *this;
+		};
+		vector3& operator+=(const vector3& rhs) {
+			components[0] += rhs.getX();
+			components[1] += rhs.getY();
+			components[2] += rhs.getZ();
+			return *this;
+		};
+		vector3& operator-=(const vector3& rhs) {
+			components[0] -= rhs.getX();
+			components[1] -= rhs.getY();
+			components[2] -= rhs.getZ();
+			return *this;
+		};
+		const vector3 operator+ (const vector3& rhs) const {
+			vector3 result = *this; // copy this
+			result += rhs;
+			return result;
+		};
+		const vector3 operator- (const vector3& rhs) const {
+			vector3 result = *this; // copy self
+			result -= rhs;
+			return result;
+		};
+};
+
+#endif
